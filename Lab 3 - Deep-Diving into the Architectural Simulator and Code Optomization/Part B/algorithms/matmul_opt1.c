208e22222ae231df97eea07cfef063d251542c07
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2022-11-09 21:56:03 -0800

    matrix optimization reorder, transpose, vectorize, blocking, and report

--- Lab 3 - Deep-Diving into the Architectural Simulator and Code Optomization/Part B/algorithms/matmul_opt1.c
+++ Lab 3 - Deep-Diving into the Architectural Simulator and Code Optomization/Part B/algorithms/matmul_opt1.c
@@ -1,21 +1,115 @@
 #include "matmul_opt1.h"
 #include <string.h>
+#include <stdlib.h>
 
-// Transposition
+// #define VECTOR_SIZE 32
+#define BLOCK_SIZE 64
+
 void matmul_opt1(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
-    unsigned long long int new_mat_b [matrix_size * matrix_size];
+    // BLOCKING
+    for (unsigned long long int i = 0; i < matrix_size; i += BLOCK_SIZE) {
+         for (unsigned long long int j = 0; j < matrix_size; j += BLOCK_SIZE) {
+             mat_c[i*matrix_size+j] = 0;
+             for (unsigned long long int k = 0; k < matrix_size; k += BLOCK_SIZE) {
+                 for (unsigned long long int x = i; x < ((i + BLOCK_SIZE) > matrix_size ? matrix_size : (i + BLOCK_SIZE)); x++) {
+                     for (unsigned long long int y = j; y < ((j + BLOCK_SIZE) > matrix_size ? matrix_size : (j + BLOCK_SIZE)); y++) {
+                         for (unsigned long long int z = k; z < ((k + BLOCK_SIZE) > matrix_size ? matrix_size : (k + BLOCK_SIZE)); z++) {
 
-    for (int i=0; i < matrix_size; i++) {
-        for (int j=0; j < matrix_size; j++) {
-            new_mat_b[i * matrix_size + j] = mat_b[j * matrix_size + i];
-        }
-    }
+                             mat_c[x*matrix_size + y] += mat_a[z*matrix_size + y] * mat_b[x*matrix_size  + z];
+
+                         }
+                     } 
+                 }
+             }
+         }
+     }
+
+    // REORDER
+    // for(unsigned long long int j=0; j<matrix_size; j++){
+    //     for(unsigned long long int k=0; k<matrix_size; k++){
+    //         for(unsigned long long int i=0; i<matrix_size; i++){
+    //             mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
+    //         }
+    //     }
+    // }
+
+    // Transpose
+    // unsigned long long int new_mat_b [matrix_size * matrix_size];
+
+    // for (int i=0; i < matrix_size; i++) {
+    //     for (int j=0; j < matrix_size; j++) {
+    //         new_mat_b[j * matrix_size + i] = mat_b[i * matrix_size + j];
+    //     }
+    // }
     
-    for (int i = 0; i < matrix_size; i++) {
-        for (int j = 0; j < matrix_size; j++) {
-            for (int k = 0; k < matrix_size; k++) {
-                mat_c[i * matrix_size + j] += mat_a[i * matrix_size + k] * new_mat_b[j * matrix_size + k];
-            }
-        }
-    }
+    // // reorder
+    // for (int j = 0; j < matrix_size; j++) {
+    //     for (int k = 0; k < matrix_size; k++) {
+    //         for (int i = 0; i < matrix_size; i++) {
+    //             mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
+    //         }
+    //     }
+    // }
 }
+
+// Vectorize
+// a vector of 256 / 32 = 8 floats
+// typedef float vec __attribute__ (( vector_size(32) ));
+
+// // a helper function that allocates n vectors and initializes them with zeros
+// vec* alloc(int n) {
+//     vec* ptr = (vec*) aligned_alloc(32, 32 * n);
+//     memset(ptr, 0, 32 * n);
+//     return ptr;
+// }
+
+// void matmul_opt1(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
+//     int nB = (matrix_size + 7) / 8; // number of 8-element vectors in a row (rounded up)
+
+//     vec *mat_a_new = alloc(matrix_size * nB);
+//     vec *mat_b_new = alloc(matrix_size * nB);
+
+//     // move both matrices to the aligned region
+//     for (int i = 0; i < matrix_size; i++) {
+//         for (int j = 0; j < matrix_size; j++) {
+//             mat_a_new[i * nB + j / 8][j % 8] = mat_a[i * matrix_size + j];
+//             mat_b_new[i * nB + j / 8][j % 8] = mat_b[j * matrix_size + i]; // <- b is still transposed
+//         }
+//     }
+
+//     for (int i = 0; i < matrix_size; i++) {
+//         for (int j = 0; j < matrix_size; j++) {
+//             vec s = {}; // initialize the accumulator with zeros
+
+//             // vertical summation
+//             for (int k = 0; k < nB; k++)
+//                 s += mat_a_new[i * nB + k] * mat_b_new[j * nB + k];
+            
+//             // horizontal summation
+//             for (int k = 0; k < 8; k++)
+//                 mat_c[i * matrix_size + j] += s[k];
+//         }
+//     }
+
+//     free(mat_a_new);
+//     free(mat_b_new);
+// }
+
+// Transpose
+// void matmul_opt1(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
+//     unsigned long long int new_mat_b [matrix_size * matrix_size];
+
+//     for (int i=0; i < matrix_size; i++) {
+//         for (int j=0; j < matrix_size; j++) {
+//             new_mat_b[j * matrix_size + i] = mat_b[i * matrix_size + j];
+//         }
+//     }
+    
+//     for (int i = 0; i < matrix_size; i++) {
+//         for (int j = 0; j < matrix_size; j++) {
+//             for (int k = 0; k < matrix_size; k++) {
+//                 mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
+//             }
+//         }
+//     }
+// }
--- Lab 3 - Deep-Diving into the Architectural Simulator and Code Optomization/Part B/algorithms/matmul_opt2.c
+++ Lab 3 - Deep-Diving into the Architectural Simulator and Code Optomization/Part B/algorithms/matmul_opt2.c
@@ -1,22 +1,21 @@
 #include "matmul_opt2.h"
 
-#define CACHE_LINE_SIZE 64
-
 void matmul_opt2(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
-    const int block_size = CACHE_LINE_SIZE / sizeof(unsigned long long int);
-    
-    for(int jj=0; jj < matrix_size; jj += block_size){
-        for(int kk=0; kk < matrix_size; kk += block_size){
-                for(int i=0; i < matrix_size; i++){
-                        for(int j=jj; j < ((jj + block_size) > matrix_size ? matrix_size : (jj + block_size)); j++){
-                                unsigned long long int temp = 0;
-                                
-                                for(int k=kk; k < ((kk + block_size) > matrix_size ? matrix_size : (kk + block_size)); k++){
-                                        temp += mat_a[i * matrix_size + k] * mat_b[k*matrix_size + j];
-                                }
-                                mat_c[i * matrix_size + j] += temp;
-                        }
-                }
+    // TRANSPOSE
+    unsigned long long int new_mat_b [matrix_size * matrix_size];
+
+    for (int i=0; i < matrix_size; i++) {
+        for (int j=0; j < matrix_size; j++) {
+            new_mat_b[j * matrix_size + i] = mat_b[i * matrix_size + j];
         }
-}
+    }
+    
+    // REORDER
+    for (int j = 0; j < matrix_size; j++) {
+        for (int k = 0; k < matrix_size; k++) {
+            for (int i = 0; i < matrix_size; i++) {
+                mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
+            }
+        }
+    }
 }
