32b0fe68a50dfdc1389f35d9eb65470369f57d9c
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2022-11-13 11:27:04 -0800

    fixed matrix multiplication optimization with blocked multiplication

--- Lab 3 - Deep-Diving into the Architectural Simulator and Code Optomization/Part B/algorithms/matmul_opt2.c
+++ Lab 3 - Deep-Diving into the Architectural Simulator and Code Optomization/Part B/algorithms/matmul_opt2.c
@@ -1,21 +1,26 @@
 #include "matmul_opt2.h"
+#include <string.h>
+#include <stdlib.h>
+
+#define CACHE_LINE_SIZE 64
+#define BLOCK_SIZE CACHE_LINE_SIZE/sizeof(unsigned long long int)
 
 void matmul_opt2(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
-    // TRANSPOSE
-    unsigned long long int new_mat_b [matrix_size * matrix_size];
+    // BLOCKING
+    for (unsigned long long int i = 0; i < matrix_size; i += BLOCK_SIZE) {
+         for (unsigned long long int j = 0; j < matrix_size; j += BLOCK_SIZE) {
+             mat_c[i*matrix_size+j] = 0;
+             for (unsigned long long int k = 0; k < matrix_size; k += BLOCK_SIZE) {
+                 for (unsigned long long int x = i; x < ((i + BLOCK_SIZE) > matrix_size ? matrix_size : (i + BLOCK_SIZE)); x++) {
+                     for (unsigned long long int y = j; y < ((j + BLOCK_SIZE) > matrix_size ? matrix_size : (j + BLOCK_SIZE)); y++) {
+                         for (unsigned long long int z = k; z < ((k + BLOCK_SIZE) > matrix_size ? matrix_size : (k + BLOCK_SIZE)); z++) {
 
-    for (int i=0; i < matrix_size; i++) {
-        for (int j=0; j < matrix_size; j++) {
-            new_mat_b[j * matrix_size + i] = mat_b[i * matrix_size + j];
-        }
-    }
-    
-    // REORDER
-    for (int j = 0; j < matrix_size; j++) {
-        for (int k = 0; k < matrix_size; k++) {
-            for (int i = 0; i < matrix_size; i++) {
-                mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
-            }
-        }
-    }
+                             mat_c[x*matrix_size + y] += mat_a[z*matrix_size + y] * mat_b[x*matrix_size  + z];
+
+                         }
+                     } 
+                 }
+             }
+         }
+     }
 }
